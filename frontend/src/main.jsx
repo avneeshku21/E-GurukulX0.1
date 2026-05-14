2d2e6212baec20f7cfe7a821f6bc2f3b60fe30d2
Avneesh Kumar <kaviikumar010@gmail.com>
2026-05-14 17:56:11 +0530

    14 May 2026 Done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider }  from './context/AuthContext.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProvider>
         <ThemeProvider>
           <App />
+          <Analytics />
         </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
